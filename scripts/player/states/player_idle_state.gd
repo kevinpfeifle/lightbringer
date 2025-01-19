edcf6a15b905edf6822532cd9085a53bc67c316b
Kevin Pfeifle <kevinpfeifle@outlook.com>
2025-01-19 13:51:43 -0500

    added a player idle state

---
+++ scripts/player/states/player_idle_state.gd
@@ -0,0 +1,11 @@
+class_name PlayerIdleState
+extends PlayerState
+
+func _ready() -> void:
+	state_name = "idle"
+
+func enter(args: Array) -> void:
+	super(args)
+	
+	if !skip_state:
+		parent.animation_player.queue("idle")
