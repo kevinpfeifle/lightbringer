8f6cf330c00019923390c98870f33c3d1d4fd894
Kevin Pfeifle <kevinpfeifle@outlook.com>
2025-01-21 10:40:43 -0500

    added a modular health component

---
+++ scripts/components/health_component.gd
@@ -0,0 +1,36 @@
+class_name HealthComponent
+extends Node
+
+@export var i_frame_timer: Timer
+@export var max_health: float
+
+signal damaged(amount: float, source: Node, power: int, direction: Vector2)
+signal healed(amount: float)
+signal dead()
+
+var alive: bool = true
+var current_health: float
+var has_i_frames: bool = false
+
+func _ready() -> void:
+	current_health = max_health
+	i_frame_timer.timeout.connect(_on_i_frames_timeout)
+
+func _on_i_frames_timeout() -> void:
+	has_i_frames = false
+
+func damage(amount: float, source: Node, power: int, direction: Vector2) -> void:
+	if !has_i_frames:
+		print("collided with enemy")
+		has_i_frames = true
+		i_frame_timer.start()
+		current_health -= amount
+		damaged.emit(amount, source, power, direction)
+		if current_health < 0:
+			print("dead")
+			alive = false
+			dead.emit()
+
+func heal(amount: float) -> void:
+	current_health += amount
+	healed.emit(amount)
