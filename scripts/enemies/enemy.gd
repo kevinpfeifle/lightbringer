74a989424a67e7b94f72774841c1deee4d4a01b8
Kevin Pfeifle <kevinpfeifle@outlook.com>
2025-01-23 00:38:28 -0500

    gave test enemy health component and death animation

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -2,6 +2,11 @@ class_name Enemy
 extends CharacterBody2D
 
 @export var animation_player: AnimationPlayer
+@export var health_component: HealthComponent
 
 func _ready() -> void:
 	animation_player.play("alive")
+	health_component.dead.connect(_on_dead)
+
+func _on_dead():
+	animation_player.play("death")
