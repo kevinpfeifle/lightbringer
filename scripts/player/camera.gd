a31b8bd68e1f74ee52575c5ea81f5c3d9e7b66a6
Kevin Pfeifle <kevinpfeifle@outlook.com>
2025-01-27 13:21:51 -0500

    camera improvements so it always starts within the scene

--- scripts/player/camera.gd
+++ scripts/player/camera.gd
@@ -6,9 +6,17 @@ var target_offset: Vector2
 
 func _ready() -> void:
 	origin = offset
+	print(global_position)
+	# Force the camera to align with its limits
+	if position_smoothing_enabled: 
+		# If using smoothing, update immediately
+		reset_smoothing()
+	force_update_scroll()
 
 func _process(delta) -> void:
 	offset = offset.move_toward(target_offset, 800 * delta)
+	# print(offset)
+	# print(global_position)
 
 func _pan_camera(distance) -> void:
 	target_offset = distance
