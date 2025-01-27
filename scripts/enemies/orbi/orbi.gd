ff8c2cf8002f9295a4cee3c5a8d8eb2d1ce91d8d
Kevin Pfeifle <kevinpfeifle@outlook.com>
2025-01-26 22:35:30 -0500

    added functional orbi chase state using navigation agent

--- scripts/enemies/orbi/orbi.gd
+++ scripts/enemies/orbi/orbi.gd
@@ -1,24 +1,25 @@
 class_name Orbi
 extends Enemy
 
-@export var chase_leash_distance: int
+@export var chase_area: Area2D
 @export var chase_speed: float
 @export var detection_area: Area2D
 @export var home_point: Marker2D
-@export var hurt_timer: Timer
 @export var nav_agent: NavigationAgent2D
 @export var speed: float
 @export var state_machine: StateMachine
 @export var wander_timer: Timer # A timer for 5 seconds, it is the max wander duration.
 @export var wander_area: Area2D
-@export var wander_leash_distance: int
 @export var wait_timer: Timer # A timer for how long to wait between wanders. Max 5 seconds.
 
 var is_hurt: bool = false
-var player_dectected: bool = false
+var player_detected: bool = false
+var player_noticed: bool = false
 
 func _ready() -> void:
 	super()
+	chase_area.body_entered.connect(_on_chase_area_entered)
+	chase_area.body_exited.connect(_on_chase_area_exited)
 	detection_area.body_entered.connect(_on_detection_area_entered)
 	detection_area.body_exited.connect(_on_detection_area_exited)
 
@@ -29,11 +30,20 @@ func _physics_process(_delta: float) -> void:
 func _on_damaged(_amount: float, _source: Node, _power: int, direction: Vector2):
 	knockback_component.initialize_knockback(direction)
 
+func _on_chase_area_entered(body: Node2D) -> void:
+	if body is Player:
+		player_detected = true
+
+func _on_chase_area_exited(body: Node2D) -> void:
+	if body is Player:
+		player_detected = false
+
 func _on_detection_area_entered(body: Node2D) -> void:
 	if body is Player:
-		player_dectected = true
-		state_machine.current_state.transition.emit("chase", [state_machine.current_state.state_name])
+		player_noticed = true
+		if state_machine.current_state.state_name != "chase":
+			state_machine.current_state.transition.emit("chase", [state_machine.current_state.state_name, body])
 
 func _on_detection_area_exited(body: Node2D) -> void:
 	if body is Player:
-		player_dectected = false
+		player_noticed = false
