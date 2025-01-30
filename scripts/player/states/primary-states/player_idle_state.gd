e5642b5f8b1560702f92cca50682429d21454dad
Kevin Pfeifle <kevinpfeifle@outlook.com>
2025-01-30 00:55:42 -0500

    commented out camera panning until i can get it functioning with the camera limits

--- scripts/player/states/primary-states/player_idle_state.gd
+++ scripts/player/states/primary-states/player_idle_state.gd
@@ -9,40 +9,40 @@ var direction: LookDirection
 
 func _ready() -> void:
 	state_name = "idle"
-	look_timer.timeout.connect(_on_look_timer_timeout)
+	# look_timer.timeout.connect(_on_look_timer_timeout)
 
 func enter(args: Array) -> void:
 	super(args)
 
 func exit(new_state: StringName) -> void:
 	super(new_state)
-	parent.camera.center_camera()
+	# parent.camera.center_camera()
 
-func update(_delta) -> void:
-	if Input.is_action_just_pressed("player_up"):
-		look_timer.start()
-		direction = LookDirection.UP
-	elif Input.is_action_just_released("player_up"):
-		if !look_timer.is_stopped():
-			look_timer.stop()
-		else:
-			parent.camera.center_camera()
-	if Input.is_action_just_pressed("player_down"):
-		look_timer.start()
-		direction = LookDirection.DOWN
-	elif Input.is_action_just_released("player_down"):
-		if !look_timer.is_stopped():
-			look_timer.stop()
-		else:
-			parent.camera.center_camera()
+# func update(_delta) -> void:
+# 	if Input.is_action_just_pressed("player_up"):
+# 		look_timer.start()
+# 		direction = LookDirection.UP
+# 	elif Input.is_action_just_released("player_up"):
+# 		if !look_timer.is_stopped():
+# 			look_timer.stop()
+# 		else:
+# 			parent.camera.center_camera()
+# 	if Input.is_action_just_pressed("player_down"):
+# 		look_timer.start()
+# 		direction = LookDirection.DOWN
+# 	elif Input.is_action_just_released("player_down"):
+# 		if !look_timer.is_stopped():
+# 			look_timer.stop()
+# 		else:
+# 			parent.camera.center_camera()
 
 func physics_update(delta) -> void:
 	super(delta)
 	parent.decrease_light()
 	parent.velocity.x = move_toward(parent.velocity.x, 0, parent.speed)
 
-func _on_look_timer_timeout() -> void:
-	if direction == LookDirection.UP:
-		parent.camera.look_up()
-	elif direction == LookDirection.DOWN:
-		parent.camera.look_down()
+# func _on_look_timer_timeout() -> void:
+# 	if direction == LookDirection.UP:
+# 		parent.camera.look_up()
+# 	elif direction == LookDirection.DOWN:
+# 		parent.camera.look_down()
